799516cd30c3630dbf47ce9896553ef8eb09db36
dar-ksen <ksen.vl.set@gmail.com>
2021-06-30 13:01:55 +0300

    Continue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url} receiver ${res.status}`)
+  }
   const body = await res.json();
   return body;
 }
 
-getResource('https://swapi.dev/api/planets/1/')
+getResource('https://swapi.dev/api/planets/12312341/')
   .then((body) => {
-    console.log(body)
+    console.log(body);
+  })
+  .catch((err) => {
+    console.error(err);
   })
